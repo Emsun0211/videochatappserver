d0d58c4f7c0b1e70616bd3079ffae47c1fa78594
Emsun <emsunconcept@gmail.com>
2023-05-16 10:38:41 +0100

    added the cors origin

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: "*",
+		origin: "https://videochatclient.vercel.app/",
 		methods: ["GET", "POST"],
 	},
 });
